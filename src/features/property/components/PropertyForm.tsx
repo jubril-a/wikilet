954c6a4900aafe39f031c787114583e7648ac0a9
Jubril Abdulsalam <abdulsalamjubr3@gmail.com>
2026-06-03 12:38:21 +0100

    add error message to create property on fail

--- src/features/property/components/PropertyForm.tsx
+++ src/features/property/components/PropertyForm.tsx
@@ -8,7 +8,7 @@ import Facilities from "./steps/Facilities"
 import Pricing from "./steps/Pricing"
 import Rules from "./steps/Rules"
 import Terms from "./steps/Terms"
-import { Dispatch, SetStateAction, useState } from "react"
+import { Dispatch, SetStateAction, useState, useEffect } from "react"
 import { useListingStore } from "@/src/stores/listingStore"
 import { createProperty } from "../actions"
 import { useRouter } from "next/navigation"
@@ -16,8 +16,14 @@ import { cn } from "@/src/lib/utils"
 
 export default function PropertyForm({ currentStepIndex, setCurrentStepIndex }: { currentStepIndex: number, setCurrentStepIndex: Dispatch<SetStateAction<number>>}) {
 
+    useEffect(() => {
+        reset()
+    }, [])
+
     const getPayload = useListingStore((state) => state.getPayload)
+    const reset = useListingStore((state) => state.reset)
     const [submitting, setSubmitting] = useState(false)
+    const [error, setError] = useState<string | null>(null)
     const router = useRouter()
 
     const forms = [
@@ -42,14 +48,16 @@ export default function PropertyForm({ currentStepIndex, setCurrentStepIndex }: 
     }
 
     const handleSubmit = async () => {
-
         setSubmitting(true)
+        setError(null)
         const formData = getPayload()
 
         try {
             const data = await createProperty(formData)
+            reset()
             router.push("/host/properties")
-        } catch (err) {
+        } catch (err: any) {
+            setError("Something went wrong. Please check all form fields and make sure everything is filled in correctly.")
             setSubmitting(false)
         }
     }
@@ -62,6 +70,9 @@ export default function PropertyForm({ currentStepIndex, setCurrentStepIndex }: 
                     <CurrentStep page="create" index={currentStepIndex + 1} />
                 }
             </div>
+            {error && (
+                <p className="text-red-500 text-sm my-4">{error}</p>
+            )}
             <div className="flex justify-between items-center gap-4 mt-10 max-w-150">
                 {currentStepIndex !== 0 && <button type="button" onClick={prev} className="bg-primary-1 rounded-md hover:bg-primary-2 text-white hover:text-primary-1 mb-6 py-3 cursor-pointer w-full">Prev Step</button>}
                 {currentStepIndex !== forms.length - 1 && <button type="button" onClick={next} className="bg-primary-1 rounded-md hover:bg-primary-2 text-white hover:text-primary-1 mb-6 py-3 cursor-pointer w-full">Next Step</button>}
